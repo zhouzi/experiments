3f1d946736809bc0687dd157be6bc146faeafff4
Gabin <hello@gabinaureche.com>
2016-09-23 10:05:07 +0200

    [snake-flow]: fix some lint issues

--- snake-flow/src/__test__/index.js
+++ snake-flow/src/__test__/index.js
@@ -4,7 +4,7 @@ const { createGame, tick, direction, grow } = require('../index');
 test('it should move the snake to the right', (assert) => {
   const actual = tick(createGame()).snake;
   const expected = [
-    [1, 0]
+    [1, 0],
   ];
   assert.deepEqual(actual, expected);
 });
@@ -12,7 +12,7 @@ test('it should move the snake to the right', (assert) => {
 test('it should move the snake further to the right', (assert) => {
   const actual = tick(tick(createGame())).snake;
   const expected = [
-    [2, 0]
+    [2, 0],
   ];
   assert.deepEqual(actual, expected);
 });
@@ -32,7 +32,7 @@ test('it should update direction to the top', (assert) => {
 test('it should move the snake to the bottom', (assert) => {
   const actual = tick(direction(createGame(), 'bottom')).snake;
   const expected = [
-    [0, 1]
+    [0, 1],
   ];
   assert.deepEqual(actual, expected);
 });
@@ -40,7 +40,7 @@ test('it should move the snake to the bottom', (assert) => {
 test('it should move the snake further to the bottom', (assert) => {
   const actual = tick(tick(direction(createGame(), 'bottom'))).snake;
   const expected = [
-    [0, 2]
+    [0, 2],
   ];
   assert.deepEqual(actual, expected);
 });
@@ -48,12 +48,12 @@ test('it should move the snake further to the bottom', (assert) => {
 test('it should restrict moves to the area', (assert) => {
   const game = createGame({
     snake: [
-      [10, 0]
-    ]
+      [10, 0],
+    ],
   });
   const actual = tick(game).snake;
   const expected = [
-    [0, 0]
+    [0, 0],
   ];
   assert.deepEqual(actual, expected);
 });
@@ -62,7 +62,7 @@ test('it should grow the snake', (assert) => {
   const actual = grow(createGame()).snake;
   const expected = [
     [10, 0],
-    [0, 0]
+    [0, 0],
   ];
   assert.deepEqual(actual, expected);
 });
@@ -72,8 +72,8 @@ test('it should grow a bigger snake', (assert) => {
     snake: [
       [1, 0],
       [2, 0],
-      [2, 1]
-    ]
+      [2, 1],
+    ],
   });
   const actual = grow(game).snake;
   const expected = [
