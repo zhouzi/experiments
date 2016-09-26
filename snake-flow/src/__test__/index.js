d1ccfde43cfb32e17869b838f13cb6d095a5201e
Gabin <hello@gabinaureche.com>
2016-09-26 22:25:13 +0200

    [snake-flow]: implement tick, grow, direction

--- snake-flow/src/__test__/index.js
+++ snake-flow/src/__test__/index.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const { createGame } = require('../index');
+const { createGame, tick, direction, grow } = require('../index');
 
 test('it should create a game', (assert) => {
   const actual = createGame();
@@ -11,5 +11,81 @@ test('it should create a game', (assert) => {
     ],
     food: [],
   };
+  assert.deepEqual(actual, expected);
+});
+
+test('should move the snake to the right', (assert) => {
+  const actual = tick(createGame()).snake;
+  const expected = [
+    [1, 0],
+  ];
+  assert.deepEqual(actual, expected);
+});
+
+test('should move the snake further to the right', (assert) => {
+  const actual = tick(tick(createGame())).snake;
+  const expected = [
+    [2, 0],
+  ];
+  assert.deepEqual(actual, expected);
+});
+
+test('should update the direction to bottom', (assert) => {
+  const actual = direction(createGame(), 'bottom').direction;
+  const expected = 'bottom';
+  assert.is(actual, expected);
+});
+
+test('should update the direction to top', (assert) => {
+  const actual = direction(createGame(), 'top').direction;
+  const expected = 'top';
+  assert.is(actual, expected);
+});
+
+test('should move the snake to the bottom', (assert) => {
+  const actual = tick(direction(createGame(), 'bottom')).snake;
+  const expected = [
+    [0, 1],
+  ];
+  assert.deepEqual(actual, expected);
+});
+
+test('should grow the snake', (assert) => {
+  const actual = grow(createGame()).snake;
+  const expected = [
+    [0, 0],
+    [10, 0],
+  ];
+  assert.deepEqual(actual, expected);
+});
+
+test('should not move snake out of bounds', (assert) => {
+  const game = createGame();
+  game.snake = [
+    [10, 0],
+  ];
+  const actual = tick(game).snake;
+  const expected = [
+    [0, 0],
+  ];
+  assert.deepEqual(actual, expected);
+});
+
+test('should move a big snake', (assert) => {
+  const compose = (...fns) => value => fns.reduce((acc, fn) => fn(acc), value);
+  const actions = compose(
+    grow,
+    grow,
+    tick,
+    tick,
+    game => direction(game, 'bottom'),
+    tick,
+  );
+  const actual = actions(createGame()).snake;
+  const expected = [
+    [2, 1],
+    [2, 0],
+    [1, 0],
+  ];
   assert.deepEqual(actual, expected);
 });
--- snake-flow/src/index.js
+++ snake-flow/src/index.js
@@ -21,6 +21,84 @@ function createGame(): Game {
   };
 }
 
+function withinBounds(n: number, min: number, max: number): number {
+  if (n > max) {
+    return min;
+  }
+
+  if (n < min) {
+    return max;
+  }
+
+  return n;
+}
+
+function createPointWithinBounds(point: Point, bounds: Point): Point {
+  const [x, y] = point;
+  const [maxX, maxY] = bounds;
+
+  return [
+    withinBounds(x, 0, maxX),
+    withinBounds(y, 0, maxY),
+  ];
+}
+
+function movePoint(previousPoint: Point, dir: Direction, bounds: Point, factor: number): Point {
+  const [x, y] = previousPoint;
+  const point = (() => {
+    const forward = factor * 1;
+    const backward = factor * -1;
+
+    if (dir === 'top') {
+      return [x, y + backward];
+    }
+
+    if (dir === 'right') {
+      return [x + forward, y];
+    }
+
+    if (dir === 'bottom') {
+      return [x, y + forward];
+    }
+
+    if (dir === 'left') {
+      return [x - backward, y];
+    }
+
+    throw new Error(`unknown direction "${dir}"`);
+  })();
+  return createPointWithinBounds(point, bounds);
+}
+
+function tick(game: Game): Game {
+  const head = game.snake[0];
+  const tail = game.snake.slice(0, -1);
+  const point = movePoint(head, game.direction, game.bounds, 1);
+  const snake = [point].concat(tail);
+
+  return Object.assign({}, game, {
+    snake,
+  });
+}
+
+function direction(game: Game, dir: Direction): Game {
+  return Object.assign({}, game, {
+    direction: dir,
+  });
+}
+
+function grow(game: Game): Game {
+  const lastPoint = game.snake[game.snake.length - 1];
+  const point = movePoint(lastPoint, game.direction, game.bounds, -1);
+
+  return Object.assign({}, game, {
+    snake: game.snake.concat([point]),
+  });
+}
+
 module.exports = {
   createGame,
+  tick,
+  direction,
+  grow,
 };
